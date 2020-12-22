b0b251398954a9ffe3db613a44dc314d27573786
Hendry <h3khoza@gmail.com>
2020-12-22 23:38:53 +0000

    added page navigation

--- dist/script.js
+++ dist/script.js
@@ -12,7 +12,7 @@ Node.prototype.on = window.on = function (name, fn) {
 }
 
 NodeList.prototype.__proto__ = Array.prototype;
-NodeList.prototype.on = function(name, fn) {
+NodeList.prototype.on = function (name, fn) {
   this.forEach(function (elem, i) {
     elem.on(name, fn)
   });
@@ -41,3 +41,31 @@ document.on('keydown', function (e) {
     closeModal();
   }
 });
+
+$('.btn--scroll-to').on('click', function (e) {
+  const s1cords = $('#section--1').getBoundingClientRect();
+  window.scrollTo({
+    left: s1cords.left + window.pageYOffset,
+    top: s1cords.top + window.pageYOffset,
+    behavior: 'smooth'
+  })
+
+})
+
+// $$('.nav__link').forEach(function (el) {
+//   el.on('click', function (e) {
+//     e.preventDefault();
+//     const id = this.getAttribute('href');
+//     $(id).scrollIntoView({behavior: 'smooth'})
+//   })
+// })
+
+$('.nav__links').on('click', function(e){
+  e.preventDefault();
+
+  if(e.target.classList.contains('nav__link')){
+    const id = e.target.getAttribute('href');
+    $(id).scrollIntoView({behavior: 'smooth'})
+  }
+    
+})
