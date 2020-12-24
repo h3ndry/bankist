f95b2b4b2bd2b492ee22690c4c4bead3082b2fdf
Hendry <h3khoza@gmail.com>
2020-12-24 15:45:08 +0000

    finish with slider

--- dist/script.js
+++ dist/script.js
@@ -82,34 +82,34 @@ $('.operations__tab-container').on('click', function (e) {
 
 })
 
-$('.nav').on('mouseover', function(e){
-  if(e.target.classList.contains('nav__link')) {
+$('.nav').on('mouseover', function (e) {
+  if (e.target.classList.contains('nav__link')) {
     const link = e.target;
     const sibiling = link.closest('.nav').querySelectorAll('.nav__link')
     const logo = link.closest('.nav').querySelector('img')
 
 
-  sibiling.forEach(el => {
-    if (el !== link) el.style.opacity = 0.5;
-  })
-  logo.style.opacity = 0.5;
+    sibiling.forEach(el => {
+      if (el !== link) el.style.opacity = 0.5;
+    })
+    logo.style.opacity = 0.5;
 
   }
 
 })
 
-$('.nav').on('mouseout', function(e){
+$('.nav').on('mouseout', function (e) {
 
-  if(e.target.classList.contains('nav__link')) {
+  if (e.target.classList.contains('nav__link')) {
     const link = e.target;
     const sibiling = link.closest('.nav').querySelectorAll('.nav__link')
     const logo = link.closest('.nav').querySelector('img')
 
 
-  sibiling.forEach(el => {
-    if (el !== link) el.style.opacity = 1;
-  })
-  logo.style.opacity = 1;
+    sibiling.forEach(el => {
+      if (el !== link) el.style.opacity = 1;
+    })
+    logo.style.opacity = 1;
 
   }
 
@@ -126,11 +126,11 @@ $('.nav').on('mouseout', function(e){
 // })
 //
 
-const stickyNav = function(entries) {
+const stickyNav = function (entries) {
 
   const [entry] = entries
-// isIntersecting
-  if(!entry.isIntersecting)
+  // isIntersecting
+  if (!entry.isIntersecting)
     $('.nav').classList.add('sticky');
   else
     $('.nav').classList.remove('sticky');
@@ -146,7 +146,13 @@ const headerObserver = new IntersectionObserver(stickyNav, {
 headerObserver.observe($('.header'))
 
 
-const revealSection = function(entries, observer) {
+const revealSection = function (entries, observer) {
+  const [entry] = entries
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
 
 }
 
@@ -155,7 +161,98 @@ const sectionObserver = new IntersectionObserver(revealSection, {
   threshold: 0.15
 })
 
-$$('.section').forEach(function(section) {
+$$('.section').forEach(function (section) {
   sectionObserver.observe(section);
-  section.classList.add('section--hidden');
+  // section.classList.add('section--hidden');
+})
+
+// Lazzy loading images
+
+const loadImg = function (entries, observer) {
+  const [entry] = entries
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.src = entry.target.dataset.src
+
+  entry.target.on('load', function () {
+    entry.target.classList.remove('lazy-img');
+
+  })
+
+  observer.unobserve(entry.target)
+}
+
+const imgTargets = $$('img[data-src]');
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: '200px'
+})
+
+imgTargets.forEach(img => imgObserver.observe(img));
+
+// Slide Emoji
+
+const slides = $$('.slide');
+
+const btnRight = $('.slider__btn--right');
+const btnLeft = $('.slider__btn--left');
+let curSlide = 0;
+const maxSlide = slides.length;
+const dotsContainer = $('.dots');
+
+
+const creatDots = function () {
+  slides.forEach(function (_, i) {
+    dotsContainer.insertAdjacentHTML(
+      'beforeend',
+      `<button class="dots__dot" data-slide="${i}"></button>`
+    );
+
+  })
+}
+
+const activateDot = function(slide) {
+  $$('.dots__dot').forEach(dot => dot.classList.remove('dots__dot--active'));
+  $(`.dots__dot[data-slide="${slide}"]`).classList.add('dots__dot--active')
+}
+
+creatDots();
+const goToSlide = function(n) {
+  slides.forEach((s, i) => (s.style.transform = `translateX(${100 * (i - n)}%)`));
+}
+
+slides.forEach((s, i) => (s.style.transform = `translateX(${100 * i}%)`));
+
+btnRight.on('click', function () {
+
+  if (curSlide === maxSlide - 1)
+    curSlide = 0;
+  else
+    curSlide++
+  // slides.forEach((s, i) => (s.style.transform = `translateX(${100 * (i - curSlide)}%)`));
+  goToSlide(curSlide)
+  activateDot(curSlide)
+})
+
+btnLeft.on('click', function () {
+  //
+
+  if (curSlide === 0)
+    curSlide = maxSlide - 1;
+  else
+    curSlide--
+
+  // slides.forEach((s, i) => (s.style.transform = `translateX(${100 * (i - curSlide)}%)`));
+  goToSlide(curSlide)
+  activateDot(curSlide)
+})
+
+dotsContainer.on('click', function(e){
+  if(e.target.classList.contains('dots__dot')){
+    goToSlide(e.target.dataset.slide)
+    activateDot(e.target.dataset.slide)
+  }
 })
